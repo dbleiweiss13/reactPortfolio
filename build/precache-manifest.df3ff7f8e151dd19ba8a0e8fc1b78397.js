cd7a8c7b52ca2b0eddb1bf95d48c32a38ecef3b5
David Bleiweiss <dbleiweiss123@gmail.com>
2021-03-16 20:01:53 -0400

    portfolio changes

---
+++ build/precache-manifest.df3ff7f8e151dd19ba8a0e8fc1b78397.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e548dc7c5dc6452bd453b105d95b19b",
+    "url": "/reactPortfolio/./index.html"
+  },
+  {
+    "revision": "dec917925489efb4913e",
+    "url": "/reactPortfolio/./static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "0f13e2e122915ed9d1f4",
+    "url": "/reactPortfolio/./static/css/main.458288ea.chunk.css"
+  },
+  {
+    "revision": "dec917925489efb4913e",
+    "url": "/reactPortfolio/./static/js/2.60d9b25a.chunk.js"
+  },
+  {
+    "revision": "0f13e2e122915ed9d1f4",
+    "url": "/reactPortfolio/./static/js/main.dd7df03a.chunk.js"
+  },
+  {
+    "revision": "cd1d2cf495cde840b038",
+    "url": "/reactPortfolio/./static/js/runtime~main.b0242bc8.js"
+  },
+  {
+    "revision": "45755ac094a61562f2001c7b34ccf9c6",
+    "url": "/reactPortfolio/./static/media/employee.45755ac0.png"
+  },
+  {
+    "revision": "b6c3845c9cacd5afbd8f6af803ae6c3e",
+    "url": "/reactPortfolio/./static/media/fitness.b6c3845c.png"
+  },
+  {
+    "revision": "87567a27afc1d809872b78500f63992a",
+    "url": "/reactPortfolio/./static/media/hangdout.87567a27.png"
+  },
+  {
+    "revision": "ae9e8e2b335a372f62d47a39ba536582",
+    "url": "/reactPortfolio/./static/media/lyric.ae9e8e2b.png"
+  },
+  {
+    "revision": "9f46dc9ae0901e352bd331c78ae90528",
+    "url": "/reactPortfolio/./static/media/note.9f46dc9a.png"
+  },
+  {
+    "revision": "8e76159ff7ae00a3fcbf207a93752a1a",
+    "url": "/reactPortfolio/./static/media/weather.8e76159f.png"
+  }
+]);
